ed8a74a6508345f75d2a4fd151d59169781be013
artemnedbailo <artemnedb@icloud.com>
2020-03-12 23:42:15 +0200

    add url verification to test_main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,7 +7,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.driver.find_element(*MainPageLocator.LOGIN_LINK)
         link.click()
-        return LoginPage(driver=self.driver, link=self.driver.current_url)
+        # return LoginPage(driver=self.driver, link=self.driver.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocator.LOGIN_LINK), 'Login link is not presented'
--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,8 @@ from .pages.locators import LoginPageLocators
 def test_guest_can_go_to_login_page(driver):
     page = MainPage(driver, MainPageLocator.LINK)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(driver, driver.current_url)
     login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(driver):
