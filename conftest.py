1ab011ce3bf47348a3bda0b1b045a97ab277cb64
artemnedbailo <artemnedb@icloud.com>
2020-03-12 20:53:58 +0200

    add login_page.py

--- conftest.py
+++ conftest.py
@@ -2,15 +2,11 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='language',
                      help='Choose language: en, ru, fr and etc')
-
-
-
 
 @pytest.fixture(scope="function")
 def driver(request):
@@ -20,7 +16,7 @@ def driver(request):
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': 'language'})
         print("\nstart chrome browser for test..")
-        driver = webdriver.Chrome(executable_path='C:/fl/firefox.chrome/chromedriver_79.exe', options=options)
+        driver = webdriver.Chrome(executable_path='C:/werq/brow/chromedriver.exe', options=options)
     # elif browser_name == "firefox":
     #     print("\nstart firefox browser for test..")
     #     browser = webdriver.Firefox(options=options,)
